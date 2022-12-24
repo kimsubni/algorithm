0e26886ed0cd2063cfe42cd2a61b27e785c88c0e
Sukong <sukong0507@gmail.com>
2022-12-24 17:10:20 +0900

    feat : 다른사람 풀이 추가

--- Programmers/[Programmers] 햄버거만들기.js
+++ Programmers/[Programmers] 햄버거만들기.js
@@ -25,3 +25,17 @@ function solution(ingredient) {
 }
 
 console.log(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
+
+function solution(ingredient) {
+  let count = 0;
+
+  for (let i = 0; i < ingredient.length; i++) {
+    if (ingredient.slice(i, i + 4).join("") === "1231") {
+      count++;
+      ingredient.splice(i, 4);
+      i -= 3;
+    }
+  }
+
+  return count;
+}
