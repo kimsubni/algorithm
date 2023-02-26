3cb360cc2be88fd57004947d847b242a9c3347fb
Sukong <sukong0507@gmail.com>
2023-02-26 18:32:27 +0900

    feat : 버블소트

--- Baekjoon/Sliver/[BOJ] 2751_수 정렬하기2.js
+++ Baekjoon/Sliver/[BOJ] 2751_수 정렬하기2.js
@@ -1,16 +1,16 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-// const input = `5
-// 5
-// 4
-// 3
-// 2
-// 1`
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
 //   .trim()
 //   .split("\n");
+const input = `5
+5
+4
+3
+2
+1`
+  .trim()
+  .split("\n");
 
 const N = Number(input[0]);
 const arr = [];
