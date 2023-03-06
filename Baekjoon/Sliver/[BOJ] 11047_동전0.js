6008f7ec494bd284b7f151f19684011806547c0a
Sukong <sukong0507@gmail.com>
2023-03-06 15:25:34 +0900

    feat : 동전0

---
+++ Baekjoon/Sliver/[BOJ] 11047_동전0.js
@@ -0,0 +1,36 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+// const input = `10 4790
+// 1
+// 5
+// 10
+// 50
+// 100
+// 500
+// 1000
+// 5000
+// 10000
+// 50000`
+//   .trim()
+//   .split("\n");
+
+const [N, K] = input[0].split(" ").map(Number);
+
+const arr = [];
+for (let i = 1; i < N + 1; ++i) {
+  arr.push(Number(input[i]));
+}
+
+let count = 0;
+let tmp = K;
+for (let i = N - 1; i >= 0; --i) {
+  while (tmp >= arr[i]) {
+    tmp -= arr[i];
+    count++;
+  }
+}
+console.log(count);
