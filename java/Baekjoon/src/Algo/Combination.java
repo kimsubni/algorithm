f1b6d8efa343257c6896c04ae27d32a845dd3f31
Sukong <sukong0507@gmail.com>
2023-03-31 21:55:13 +0900

    feat : Combination

---
+++ java/Baekjoon/src/Algo/Combination.java
@@ -0,0 +1,28 @@
+package Algo;
+
+import java.util.Arrays;
+
+public class Combination {
+    static int[] arr;
+    static int[] numbers;
+    static int N = 5;
+    static int R = 3;
+
+    public static void main(String[] args) {
+        arr = new int[] { 1, 2, 3, 4, 5 };
+        numbers = new int[R];
+        comb(0, 0);
+    }
+
+    public static void comb(int start, int cnt) {
+        if (cnt == R) {
+            System.out.println(Arrays.toString(numbers));
+            return;
+        }
+
+        for (int i = start; i < N; ++i) {
+            numbers[cnt] = arr[i];
+            comb(i + 1, cnt + 1);
+        }
+    }
+}
