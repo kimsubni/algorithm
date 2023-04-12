e3ab429285c8199ac695ff822e94e1305f4c3b00
Sukong <sukong0507@gmail.com>
2023-04-13 01:01:46 +0900

    feat : 주유소

---
+++ java/Baekjoon/src/Silver/BOJ_20300_서강근육맨.java
@@ -0,0 +1,14 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_20300_서강근육맨 {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+    }
+}
