7a05d421a9aa06d4e27fa63018d2beb61e330fcc
Sukong <sukong0507@gmail.com>
2023-04-22 15:20:45 +0900

    feat : 포도주시식

--- java/Baekjoon/src/Silver/BOJ_2579_계단오르기.java
+++ java/Baekjoon/src/Silver/BOJ_2579_계단오르기.java
@@ -18,12 +18,14 @@ public class BOJ_2579_계단오르기 {
         for (int i = 1; i < N + 1; ++i) {
             step[i] = Integer.parseInt(br.readLine());
         }
+        int max = 0;
         dp[0] = 0;
         dp[1] = step[1];
         if (N >= 2)
             dp[2] = step[1] + step[2];
         if (N >= 3)
             dp[3] = Math.max(step[1], step[2]) + step[3];
+
         for (int i = 4; i < N + 1; ++i) {
             dp[i] = Math.max(dp[i - 3] + step[i - 1], dp[i - 2]) + step[i];
         }
