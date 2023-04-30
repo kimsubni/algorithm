919c759f93672053d20c33b8b0139a13522f1963
Sukong <sukong0507@gmail.com>
2023-05-01 01:22:18 +0900

    feat : 연속된펄스부분수열의 합

---
+++ java/Programmers/Lv.3/Programmers_연속펄스부분수열의합.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class Programmers_연속펄스부분수열의합 {
+    public long maxMinusMin(long[] sum) {
+        long max = -100001;
+        long min = 100001;
+        for (int i = 0; i < sum.length; i++) {
+            if (max < sum[i])
+                max = sum[i];
+            if (min > sum[i])
+                min = sum[i];
+        }
+        return Math.abs(max - min);
+    }
+
+    public long solution(int[] sequence) {
+        long answer = 0;
+        long[] sum = new long[sequence.length + 1];
+        for (int i = 1; i < sum.length; i++) {
+            if (i % 2 == 0) {
+                sum[i] = sum[i - 1] + (long) sequence[i - 1] * -1;
+            } else {
+                sum[i] = sum[i - 1] + (long) sequence[i - 1];
+            }
+        }
+        answer = maxMinusMin(sum);
+        return answer;
+    }
+}
