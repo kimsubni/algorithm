5f9191aac074999528a90f90778172d37f3ac773
Sukong <sukong0507@gmail.com>
2023-05-29 11:34:49 +0900

    Feat : 테이블해시함수

--- java/Programmers/Lv.2/Programmers_테이블해시함수.java
+++ java/Programmers/Lv.2/Programmers_테이블해시함수.java
@@ -1,14 +1,26 @@
 
 import java.util.*;
 
-public class Programmers_테이블해시함수 {
+class Programmers_테이블해시함수 {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
-        Arrays.sort(data, (o1, o2) -> {return (o1[col-1] == o2[col-1] ? o2[0] - o1[0] : o1[col-1] - o2[col-1]);});
-        for(int i = row_begin - 1 ; i < row_end; i++){
-            int sum = 0;
-            for(int column : data[i]) sum += (column % (i + 1));
-            answer ^= sum;
+        Arrays.sort(data, (o1, o2) -> {
+            if (o1[col - 1] == o2[col - 1]) {
+                return o2[0] - o1[0];
+            }
+            return o1[col - 1] - o2[col - 1];
+        });
+        int[] sum = new int[row_end - row_begin + 1];
+        int idx = 0;
+        for (int i = row_begin - 1; i < row_end; ++i) {
+            for (int j = 0; j < data[0].length; ++j) {
+                sum[idx] += (data[i][j] % (i + 1));
+            }
+            idx++;
+        }
+        answer = sum[0];
+        for (int i = 1; i < sum.length; ++i) {
+            answer ^= sum[i];
         }
         return answer;
     }
