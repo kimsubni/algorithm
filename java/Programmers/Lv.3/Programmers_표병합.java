586e532be544fe4cd74d760fc80e06d4fb930cbf
Sukong <sukong0507@gmail.com>
2023-06-03 23:17:18 +0900

    feat : 귤고르기

--- java/Programmers/Lv.3/Programmers_표병합.java
+++ java/Programmers/Lv.3/Programmers_표병합.java
@@ -1,27 +1,27 @@
+
 import java.util.*;
 
-public class Programmers_표병합  {
+public class Programmers_표병합 {
     static int[] parent = new int[2501];
     static String[] table = new String[2501];
 
-    static public int getParent(int x) {
-        if (x == parent[x]) {
+    public int getParent(int x) {
+        if (parent[x] == x) {
             return x;
         }
         return parent[x] = getParent(parent[x]);
     }
 
-    static public void union(int a, int b) {
+    public void union(int a, int b) {
         a = getParent(a);
         b = getParent(b);
-
         if (a != b) {
             parent[b] = a;
         }
     }
 
-    static public int convertPos(int r, int c) {
-        int result = (r - 1) * 50;
+    public int convert(int r, int c) {
+        int result = 50 * (r - 1);
         return result + c;
     }
 
@@ -47,7 +47,7 @@ public class Programmers_표병합  {
                     int r = Integer.parseInt(st.nextToken());
                     int c = Integer.parseInt(st.nextToken());
                     String val = st.nextToken();
-                    int num = convertPos(r, c);
+                    int num = convert(r, c);
                     table[getParent(num)] = val;
                 }
             } else if ("MERGE".equals(order)) {
@@ -55,35 +55,39 @@ public class Programmers_표병합  {
                 int c1 = Integer.parseInt(st.nextToken());
                 int r2 = Integer.parseInt(st.nextToken());
                 int c2 = Integer.parseInt(st.nextToken());
-                int num1 = convertPos(r1, c1);
-                int num2 = convertPos(r2, c2);
+                int num1 = convert(r1, c1);
+                int num2 = convert(r2, c2);
                 int root1 = getParent(num1);
                 int root2 = getParent(num2);
+
                 if (root1 == root2)
                     continue;
-                String rootStr = table[root1].isBlank() ? table[root2] : table[root1];
+                String rootString = table[root1].isBlank() ? table[root2] : table[root1];
                 table[root1] = "";
                 table[root2] = "";
                 union(root1, root2);
-                table[root1] = rootStr;
+                table[root1] = rootString;
             } else if ("UNMERGE".equals(order)) {
                 int r = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                int num = convertPos(r, c); // 이 좌표에서
+                int num = convert(r, c);
                 int root = getParent(num);
-                String rootStr = table[root];
+                String rootString = table[root];
                 table[root] = "";
-                table[num] = rootStr;
+                table[num] = rootString;
+                List<Integer> deleteList = new ArrayList<>();
                 for (int i = 1; i <= 2500; ++i) {
-                    if (root == getParent(i)) {
-                        parent[i] = i;
+                    if (getParent(i) == root) {
+                        deleteList.add(i);
                     }
                 }
-
+                for (Integer t : deleteList) {
+                    parent[t] = t;
+                }
             } else if ("PRINT".equals(order)) {
                 int r = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                int num = convertPos(r, c);
+                int num = convert(r, c);
                 int root = getParent(num);
 
                 if (table[root].isBlank()) {
@@ -92,6 +96,7 @@ public class Programmers_표병합  {
                     result.add(table[root]);
                 }
             }
+
         }
         return result.toArray(new String[0]);
     }
