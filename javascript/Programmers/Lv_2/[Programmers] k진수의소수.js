9d905e40f4ede3c88302fdf1163da73aa8718b3e
kimsubni <yosopu0105@naver.com>
2023-08-05 23:48:40 +0900

    feat : 최소직사각형

--- javascript/Programmers/Lv_2/[Programmers] k진수의소수.js
+++ javascript/Programmers/Lv_2/[Programmers] k진수의소수.js
@@ -2,6 +2,8 @@ function solution(n, k) {
   const str = n.toString(k);
   const regex = /[1-9]*/g;
   const arr = str.match(regex).filter((e) => e !== "" && e !== "1");
+
+  console.log(arr);
   const result = [];
   arr.forEach((e) => {
     let isPrime = true;
@@ -15,3 +17,6 @@ function solution(n, k) {
   });
   return result.length;
 }
+
+console.log(solution(437674, 3));
+console.log(solution(110011, 10));
